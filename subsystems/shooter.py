19f4ec66afbfa4f013b075d9a451ee371a2e90c0
Reyna5970 <reyna.j.ayala@gmail.com>
2020-01-16 15:00:12 -0800

    pid sketch plan

--- subsystems/shooter.py
+++ subsystems/shooter.py
@@ -2,6 +2,8 @@
 
 import wpilib
 from wpilib.command import Subsystem
+from simple_pid import PID
+from encoder import encoder.ang_velocity
 
 class Shooter(Subsystem):
 	#*********Robot-Side Initialization***************
@@ -20,6 +22,24 @@ class Shooter(Subsystem):
 		pid_output = self.get_velocity()
 		self.shooter_motor.set(pid_output)
 
-	# Private function
-	def get_velocity(self):
+	# Private functions
+	def get_motor_speed(self):
+		# get current speed from encoders
 		pass
+	
+	def get_pid_velocity(self):
+		# Gets the output velocity from the pid loop
+		pid = PID(0.2, 0.1, 0.05, setpoint=10)		
+
+		while True:
+			control = pid(v)
+		pass
+
+	def calc_error(self):
+		actual_v = encoder.velocity
+		error = desired_v - actual_v
+
+	def get_velocity(self):
+		# Converts the motor speed to velocity which can be used for error calcs
+		pass
+
