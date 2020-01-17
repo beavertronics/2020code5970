c5069801aac77e703be9e76a114b9427c1266161
lollyi <lollyinman@gmail.com>
2020-01-16 17:30:15 -0800

    Working on testing shooter

--- subsystems/shooter.py
+++ subsystems/shooter.py
@@ -1,10 +1,10 @@
 # vim: set sw=4 noet ts=4 fileencoding=utf-8:
 
-import wpilib from wpilib.command 
-import Subsystem
+import wpilib
+from wpilib.command import Subsystem
 from simple_pid import PID
-from encoder import encoder.ang_velocity
-
+#from shooter_encoder import encoder.ang_velocity
+'''All values currently arbitary'''
 class Shooter(Subsystem):
 	#*********Robot-Side Initialization***************
 	def __init__(self, robot):
@@ -16,20 +16,20 @@ class Shooter(Subsystem):
 		# Assumes four PWM ports for motors
 		self.shooter_motor = wpilib.VictorSP(5)
 		
-		#Values currently arbitrary
 		#setpoint = self.get_setpoint()
-		setpoint = robot.setpoint
-		self.pid = PID(0.2, 0.1, 0.05, setpoint=setpoint)
-		pid.output_limits = (0,10)
+		#setpoint = robot.setpoint
+		self.pid = PID(0.2, 0.1, 0.05, setpoint=5)
+		self.pid.output_limits = (0,10)
 
 	def shoot(self):
 		''' Shoots the ball by controlling the flywheel motor '''
 		#XXX Need to get pid_output 
 		output = self.get_pid_output()
-		self.shooter_motor.set(output)
+		self.shooter_motor.setSpeed(output)
 
 	def get_rpm(self):
-		rpm = encoder.ang_velocity * k
+		# rpm = angular velocity * arbitrary constant
+		rpm = 5 * 4
 		return rpm
 
 	def get_pid_output(self):
@@ -37,7 +37,7 @@ class Shooter(Subsystem):
 		return output
 
 #	def get_setpoint(self, setpoint):
-#		'''Setpoint value not changing mayhaps (archaic)'''
+#		'''Setpoint value probably not changing'''
 #		#XXX maybe have user input from GUI or something
 #		# maybe also pass in as arg
 #		#Fake val
