834185db1626ebedb8ea6ac831e79cb47f6b9399
lollyi <lollyinman@gmail.com>
2020-01-16 16:26:00 -0800

    Added outline PID code

--- subsystems/shooter.py
+++ subsystems/shooter.py
@@ -1,13 +1,13 @@
 # vim: set sw=4 noet ts=4 fileencoding=utf-8:
 
-import wpilib
-from wpilib.command import Subsystem
+import wpilib from wpilib.command 
+import Subsystem
 from simple_pid import PID
 from encoder import encoder.ang_velocity
 
 class Shooter(Subsystem):
 	#*********Robot-Side Initialization***************
-	def __init__(self):
+	def __init__(self, robot):
 		'''
 		Command Dependencies:
 
@@ -15,31 +15,33 @@ class Shooter(Subsystem):
 		super().__init__()
 		# Assumes four PWM ports for motors
 		self.shooter_motor = wpilib.VictorSP(5)
+		
+		#Values currently arbitrary
+		#setpoint = self.get_setpoint()
+		setpoint = robot.setpoint
+		self.pid = PID(0.2, 0.1, 0.05, setpoint=setpoint)
+		pid.output_limits = (0,10)
 
 	def shoot(self):
 		''' Shoots the ball by controlling the flywheel motor '''
 		#XXX Need to get pid_output 
-		pid_output = self.get_velocity()
-		self.shooter_motor.set(pid_output)
+		output = self.get_pid_output()
+		self.shooter_motor.set(output)
 
-	# Private functions
-	def get_motor_speed(self):
-		# get current speed from encoders
-		pass
-	
-	def get_pid_velocity(self):
-		# Gets the output velocity from the pid loop
-		pid = PID(0.2, 0.1, 0.05, setpoint=10)		
+	def get_rpm(self):
+		rpm = encoder.ang_velocity * k
+		return rpm
 
-		while True:
-			control = pid(v)
-		pass
+	def get_pid_output(self):
+		output = self.pid(self.get_rpm())
+		return output
 
-	def calc_error(self):
-		actual_v = encoder.velocity
-		error = desired_v - actual_v
+#	def get_setpoint(self, setpoint):
+#		'''Setpoint value not changing mayhaps (archaic)'''
+#		#XXX maybe have user input from GUI or something
+#		# maybe also pass in as arg
+#		#Fake val
+#		return setpoint
 
-	def get_velocity(self):
-		# Converts the motor speed to velocity which can be used for error calcs
-		pass
+
 
