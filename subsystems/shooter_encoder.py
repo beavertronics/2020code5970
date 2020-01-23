a76937b30b60cd3de0db81413618ab2bb382f7f2
lollyi <lollyinman@gmail.com>
2020-01-22 17:16:46 -0800

    Modeline testing, fixed tabs

--- subsystems/shooter_encoder.py
+++ subsystems/shooter_encoder.py
@@ -1,8 +1,8 @@
 # vim: set sw=4 noet ts=4 fileencoding=utf-8:
 
 # Outline:
-    # need to measure encoder count
-    # convert to rpm
+	# need to measure encoder count
+	# convert to rpm
 
 import wpilib
 import math
@@ -12,9 +12,9 @@ import math
 # overwrite a function used in the original encoder class, which it looks
 # like we do not.
 class Shooter_Encoder(wpilib.Encoder):
-    
+	
 
-    def __init__(self, DIO_1, DIO_2):
+	def __init__(self, DIO_1, DIO_2):
 		super().__init__(DIO_1, DIO_2)
 		# Constants
 		pulses_per_rev = 12
@@ -22,9 +22,9 @@ class Shooter_Encoder(wpilib.Encoder):
 
 		#XXX Set this value based on the encoder’s rated Pulses per Revolution 
 		# and factor in gearing reductions following the encoder shaft
-        self.setDistancePerPulse(pulses_per_rev)
+		self.setDistancePerPulse(pulses_per_rev)
 
 	#XXX getRate returned in (units used in setDistancePerPulse) / second 
-    def get_encoder_rpm(self):
-        angular_velocity_rpm = self.getRate()
-        return angular_velocity_rpm
+	def get_encoder_rpm(self):
+		angular_velocity_rpm = self.getRate()
+		return angular_velocity_rpm
