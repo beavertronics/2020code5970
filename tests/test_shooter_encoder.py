612ab50bc04c0ef538e825b18d9c0f4fad1c3792
lollyi <lollyinman@gmail.com>
2020-01-25 16:56:20 -0800

    Added shooter encoder test

---
+++ tests/test_shooter_encoder.py
@@ -0,0 +1,37 @@
+# vim: set sw=4 noet ts=4 fileencoding=utf-8:
+import sys
+sys.path.append('./../commands')
+sys.path.append('./../subsystems')
+sys.path.append('./../')
+from unittest import mock
+import unittest
+from shooter_encoder import Shooter_Encoder
+from parametrized import ParametrizedTestCase
+import wpilib
+
+class Test_Shooter_Encoder(ParametrizedTestCase):
+	
+	# This method is run once before running ALL tests
+	@classmethod
+	def setUpClass(cls):
+		print('setUpClass()')
+	
+	# This method is run once before running EACH test
+	def setUp(self):
+		print('setUp()')
+
+	#5 is an arbitary value
+	def test_get_encoder_rpm(self):
+		print('test_get_encoder_rpm()')
+		angular_velocity_rpm = 5
+		assert(angular_velocity_rpm == 5)
+
+	def tearDown(self):
+		print('tearDown()')
+
+	@classmethod
+	def tearDownClass(cls):
+		print('tearDownClass()')
+
+if __name__ == '__main__':
+	unittest.main()
