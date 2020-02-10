00ec792a32f3aa6d909cf78952c7dd7cf7b6e8f5
lollyi <lollyinman@gmail.com>
2020-02-10 15:40:48 -0800

    Did phoenix tuner

--- subsystems/left_motors.py
+++ subsystems/left_motors.py
@@ -10,10 +10,9 @@ class Left_Motors():
 	def __init__(self):
 		#Initialize Left motors
 		# Two motors cause TuffBox 
-		#XXX Need to set these device numbers in Phoenix Tuner!
 		# Also 0 is generally reserved for PDP or something
-		left_one = (ctre.WPI_VictorSPX(1))
-		left_two = (ctre.WPI_VictorSPX(2))
+		left_one = (ctre.WPI_VictorSPX(01))
+		left_two = (ctre.WPI_VictorSPX(02))
 		self.left_motor_group = wpilib.SpeedControllerGroup(
 			left_one, left_two)
 
